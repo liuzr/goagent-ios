9c9750da04c6daaecef0c60b9d3f1eb6d7570c9e
hewigovens <hewigovens@gmail.com>
2012-06-03 14:34:34 +0800

    init repo

---
+++ goagent-ios/goagent-ios/goagent-ios/GAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAppDelegate.h
+//  goagent-ios
+//
+//  Created by hewig on 6/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ goagent-ios/goagent-iosTests/goagent_iosTests.h
@@ -0,0 +1,13 @@
+//
+//  goagent_iosTests.h
+//  goagent-iosTests
+//
+//  Created by hewig on 6/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface goagent_iosTests : SenTestCase
+
+@end
